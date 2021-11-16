42ddf6d7198d2b49ebaf3eaf0a22d6281d66efc0
Vicki20rous <vic86rous@gmail.com>
2021-11-16 17:46:16 -0600

    updated employer class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    private String location;
 
+
+    public Employer(String location) {
+       this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
